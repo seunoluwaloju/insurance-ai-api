0b68783985e8bf412fa2133c32158a709b1c986c
soluwaloju <seun.oluwaloju@siriusxm.com>
2026-04-20 22:26:48 -0500

    feat(model): Connect model to service and route. Add logging

--- app/main.py
+++ app/main.py
@@ -1,16 +1,43 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+from pathlib import Path
 from app.core.config import settings
 from app.db.session import  Base, engine
 from app.api.routes.health import router as health_router
 from app.api.routes.auth import router as auth_router
 from app.api.routes.predict import router as predict_router
+from app.core.logging import setup_logging
+import logging
+import time
+import pickle
 
-app = FastAPI(title=settings.app_name)
+MODEL_PATH = Path("app/ml/model.pkl")
+
+setup_logging()
+logger = logging.getLogger("app")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    with MODEL_PATH.open("rb") as f:
+        app.state.model = pickle.load(f)
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = round((time.time() - start_time) * 1000, 2)
+    logger.info (
+        "%s %s status=%s duration_ms=%s",
+        request.method,
+        request.url.path,
+        response.status_code,
+        duration
+    )
+
+    return response
 app.include_router(predict_router)
-
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
-
 app.include_router(health_router)
 app.include_router(auth_router)
